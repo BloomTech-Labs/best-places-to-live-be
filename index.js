07a47ecdfee1aeb4c5069ddcc108a3fc57fca049
Cai Martin <cmartin.professional@yahoo.com>
2019-08-21 12:41:43 -0400

    added npm init and took out ssl

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const cors = require("cors");
 const fs = require("fs");
 const https = require("https");
 const basic = require("./routes/basic");
-const key = fs.readFileSync("./ssl/backend.key");
-const cert = fs.readFileSync("./ssl/backend.crt");
+// const key = fs.readFileSync("./ssl/backend.key");
+// const cert = fs.readFileSync("./ssl/backend.crt");
 const app = express();
-const port = env.PORT || 3001;
-const options = {
-  key: key,
-  cert: cert
-};
+const port = process.env.PORT || 3001;
+// const options = {
+//   key: key,
+//   cert: cert
+// };
 
 //////////////////////////////////
 
@@ -23,16 +23,20 @@ app.use("/", basic);
 
 mongoose
   .connect(
-    `mongodb://${env.USER}:${env.PASS}@${env.URL}:${
-      env.PORTMONGO
+    `mongodb://${process.env.USER}:${process.env.PASS}@${process.env.URL}:${
+      process.env.PORTMONGO
     }/BPTL?authSource=admin`,
     { useNewUrlParser: true }
   )
   .then(() => console.log("Connected to MongoDB..."))
   .catch(e => console.error(`Could not connect ${e.message}`));
 
-const server = https.createServer(options, app);
+// const server = https.createServer(options, app);
+//
+// server.listen(port, () => {
+//   console.log(`server start on port ${port}`);
+// });
 
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`server start on port ${port}`);
 });
