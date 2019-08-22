4b65b46a8181abb701816267a7ec0e6df47d624e
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-08-22 15:45:27 -0700

    user creation testing

--- routes/basic.js
+++ routes/basic.js
@@ -1,22 +1,35 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const { Basic } = require("../models/basic");
+const {Basic, User} = require('../models/basic');
 
-router.get("/", (req, res) => {
-  res.json("welcome to our backend api");
+router.get('/', (req, res) => {
+  res.json('welcome to our backend api');
 });
 
-router.get("/all", async (req, res) => {
-  const result = await Basic.find().sort("name");
+router.get('/all', async (req, res) => {
+  const result = await Basic.find().sort('name');
   res.json(result);
 });
 
-router.post("/add", (req, res) => {
+router.post('/add', (req, res) => {
   const basic = new Basic({
-    name: req.body.name
+    name: req.body.name,
   });
   basic.save();
   res.json(basic);
+});
+
+router.post('/user', async (req, res) => {
+  const newUser = new User({
+    name: req.body.name || null,
+    email: req.body.email,
+  });
+  try {
+    const userCreated = await newUser.save();
+    res.json(`Welcome ${userCreated.name}!`);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
