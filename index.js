4e2ad23a87dedd40ef896fce75349daa9497e0e6
harjoth <harjoth.khara@gmail.com>
2019-08-24 13:24:01 -0700

    register post request with validation, adding partials(like jsx) for error messages with bootstrap for alert dismissals,  and included the partial inside register.ejs - ui template

--- index.js
+++ index.js
@@ -10,12 +10,15 @@ const db = require('./config/keys')
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.use(express.json());
+// app.use(express.json());
 app.use(cors());
 
 //EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
+
+// Express body parser
+app.use(express.urlencoded({ extended: true }));
 
 //Routes
 app.use('/', basic);
--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,38 @@ router.get('/register', (req, res) => {
   res.render('register');
 });
 
+// Register Handle
+router.post('/register',(req,res) => {
+ const { name, email, password, password2 } = req.body;
+ let errors = [];
+
+ // check required fields
+ if(!name || !email || !password || !password2){
+   errors.push({ msg: "Please fill in all fields" });
+ }
+
+ // check passwords match
+ if(password !== password2) {
+   errors.push({ msg: "Passwords do not match" });
+ }
+
+ // check pass length
+ if(password.length < 6) {
+   errors.push({ msg: "password must be at least 6 characters" })
+ }
+
+ if(errors.length > 0){
+  res.render('register', {
+    errors,
+    name,
+    email,
+    password,
+    password2
+  });
+ } else {
+   res.send('pass')
+ }
+
+});
+
 module.exports = router;
