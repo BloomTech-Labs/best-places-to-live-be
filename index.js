326cf7a0503182dbef7e6534b20c1798bbf0291d
harjoth <harjoth.khara@gmail.com>
2019-08-24 11:30:35 -0700

    created a dbconfig folder and added mongoURI then imported back in to index.js to connect to mongoDB

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const expressLayouts = require('express-ejs-layouts');
 const cors = require('cors');
 const basic = require('./routes/basic');
 const users = require('./routes/users');
+const db = require('./config/keys')
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -16,21 +17,14 @@ app.use(cors());
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
-// Routes
+//Routes
 app.use('/', basic);
 app.use('/users', users);
 
+//Connect to MongoDB
 mongoose
   .connect(
-        // Doesn't work!
-    // `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOSTNAME}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}?authSource=admin`,
-
-        //Locally on Compass - works!
-    "mongodb://localhost/bestplaces",
-
-      // Atlas Cloud - works! - env variables work -!
-    // `mongodb+srv://${process.env.TESTING_MONGO_USERNAME}:${process.env.TESTING_MONGO_PASSWORD}@${process.env.TESTING_MONGO_HOSTNAME}?retryWrites=true&w=majority`,
-
+     db.mongoURI,
     {useNewUrlParser: true, useFindAndModify: false},
   )
   .then(() => console.log('Connected to MongoDB...'))
