72e06fd42a1fece1073a961740630e03169ecada
harjoth <harjoth.khara@gmail.com>
2019-08-25 20:27:27 -0700

    dashboard is now a protected route and redirects to login page (ensureAuthenicated is middleware that can be added to any route in order for it to be protected)...

--- routes/basic.js
+++ routes/basic.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const {Basic} = require('../models/basic');
 const {User} = require('../models/User');
+const { ensureAuthenticated } = require('../config/auth');
 
 // Welcome Page
 router.get('/', (req, res) => {
@@ -9,7 +10,7 @@ router.get('/', (req, res) => {
 });
 
 // Dashboard
-router.get('/dashboard', (req, res) => {
+router.get('/dashboard', ensureAuthenticated, (req, res) => {
   res.render('dashboard');
 });
 
