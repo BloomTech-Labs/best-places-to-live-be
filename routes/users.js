770aff6e5b6f8f4c4b7fa169cc83d275c1ce8611
harjoth <harjoth.khara@gmail.com>
2019-08-25 19:52:57 -0700

    login cont...created our route ...when we login a post request is sent to /users/login and we have to tell that route to use the LocalStrategy...followed documentation for custom callback

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const passport = require('passport');
 
 // User model
 const User = require('../models/User');
@@ -86,6 +87,15 @@ router.post('/register',(req,res) => {
     });
  }
 
+});
+
+// Login Handle
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect: '/dashboard',
+    failureRedirect: '/users/login',
+    failureFlash: true
+  })(req, res, next);
 });
 
 module.exports = router;
