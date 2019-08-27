1665b14d30650a178de72eb9da533295117faccd
Cai Martin <cmartin.professional@yahoo.com>
2019-08-27 16:40:55 -0400

    finished first part of google auth

--- middleware/passportConfig.js
+++ middleware/passportConfig.js
@@ -6,8 +6,8 @@ passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 passport.deserializeUser((id, done) => {
-  User.findById({ id }).then(user => {
-    done(null, user.id);
+  User.findById(id).then(user => {
+    done(null, user);
   });
 });
 
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,14 @@
 const router = require("express").Router();
 const passport = require("passport");
+
+const authCheck = (req, res, next) => {
+  if (!req.user) {
+    res.send("you are not logged in");
+  } else {
+    next();
+  }
+};
+
 router.get(
   "/login",
   passport.authenticate("google", {
@@ -9,8 +18,17 @@ router.get(
     res.send("login");
   }
 );
+router.get("/logout", (req, res) => {
+  req.logOut();
+  req.session = null;
+  res.send("you have logged out");
+});
 router.get("/redirect", passport.authenticate("google"), (req, res) => {
-  res.send("Logged in!");
+  res.send(req.user);
 });
 
+router.get("/profile", authCheck, (req, res) => {
+  console.log(req.user);
+  res.send(`you are logged in hello ${req.user.profile.displayName}`);
+});
 module.exports = router;
