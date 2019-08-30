87d812ff9af44782ed6394757cff806e23d48a57
Cai Martin <cmartin.professional@yahoo.com>
2019-08-30 17:23:26 -0400

    fixed routing for cookeis

--- config/keys.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  mongoURI: `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOSTNAME}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}?authSource=admin`
-};
--- index.js
+++ index.js
@@ -7,17 +7,17 @@ const auth = require("./routes/auth");
 const passportConfig = require("./middleware/passportConfig");
 const cookie = require("cookie-session");
 const passport = require("passport");
-const expressLayouts = require('express-ejs-layouts');
-const users = require('./routes/users');
-const db = require('./config/keys')
-const flash = require('connect-flash');
-const session = require('express-session');
-
+const expressLayouts = require("express-ejs-layouts");
+const users = require("./routes/users");
+const db = require("./config/keys");
+const flash = require("connect-flash");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = process.env.PORT || 3001;
 
 // Passport Config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
 // app.use(express.json());
 app.use(cors());
@@ -30,23 +30,26 @@ app.use(
 
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(cookieParser());
 
 app.use("/", basic);
 app.use("/auth", auth);
 
 // EJS
 app.use(expressLayouts);
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // Express body parser
 app.use(express.urlencoded({ extended: true }));
 
 // Express Session
-app.use(session({
-  secret: 'secret',
-  resave: true,
-  saveUninitialized: true
-}));
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+  })
+);
 
 // Passport middleware
 app.use(passport.initialize());
@@ -57,15 +60,15 @@ app.use(flash());
 
 // Global Vars - custom middleware
 app.use((req, res, next) => {
-  res.locals.success_msg = req.flash('success_msg');
-  res.locals.error_msg = req.flash('error_msg');
-  res.locals.error = req.flash('error');
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
   next();
 });
 
 //Routes
-app.use('/', basic);
-app.use('/users', users);
+app.use("/", basic);
+app.use("/users", users);
 
 //Connect to MongoDB
 mongoose
