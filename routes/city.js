711c3838990e4a29001d7e51110efdcbb8e204aa
Christian Ipanaque <chris.ipanaque@gmail.com>
2019-08-31 19:16:08 -0700

    returns topten cities ascending order avgcomtime

--- routes/city.js
+++ routes/city.js
@@ -6,23 +6,35 @@ router.get("/", async (req, res) => {
   const cities = await City.find();
 
   res.status(200).json({
-    cities,
+    cities
+  });
+});
+
+router.get("/topten-average-commute-time", async (req, res) => {
+  const sortedByAverageCommuteTime_ASC = await City.find()
+    .sort({
+      avg_commute_time: "asc"
+    })
+    .limit(10);
+
+  res.status(200).json({
+    cities: sortedByAverageCommuteTime_ASC
   });
 });
 
 router.post("/", async (req, res) => {
-  const {name, cost_of_living, avg_commute_time} = req.body;
+  const { name, cost_of_living, avg_commute_time } = req.body;
 
   const newCity = new City({
     name,
     cost_of_living,
-    avg_commute_time,
+    avg_commute_time
   });
 
   const citySaved = await newCity.save();
 
   res.status(200).json({
-    citySaved,
+    citySaved
   });
 });
 
