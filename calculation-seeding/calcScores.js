3415e8236a509dc1cfecc99adeb6594d0a3212ee
Chase Wex <clapingmanly@hotmail.com>
2019-09-17 16:57:53 -0400

    Added new seeding to help with geojson

--- calculation-seeding/calcScores.js
+++ calculation-seeding/calcScores.js
@@ -46,14 +46,14 @@ City.find().then(cities => {
 
     //here we are gonna do a quick reasigning of geo location data
     let object = {};
-    if (city.location && city.location.latlon)
+   /*  if (city.location && city.location.latlon)
       object.location = {
         type: "Point",
         coordinates: [
           city.location.latlon.longitude,
           city.location.latlon.latitude
         ]
-      };
+      }; */
       
     const rounded_avg_commute_time = Math.round(city.avg_commute_time);
     let score = "";
