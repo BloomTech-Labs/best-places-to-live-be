db2ff6a4ab7519f75f7263b7ca7642b2f1ab0993
Isla McNeill <51002520+IslaMcN@users.noreply.github.com>
2019-11-12 10:17:58 -0800

    Revert "Cors and https cert"

--- index.js
+++ index.js
@@ -1,15 +1,63 @@
 require("dotenv").config();
 checkConfig();
-const server = require('./server');
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const passportConfig = require("./middleware/passportConfig");
+const cookie = require("cookie-session");
+const cookieParser = require("cookie-parser");
+const passport = require("passport");
+const users = require("./routes/users");
+const auth = require("./routes/auth");
+const city = require("./routes/city");
+const profile = require("./routes/profile");
+const keys = require("./config/keys");
+const https = require("https");
+const credentials = require("./config/ssl");
 
+const port = process.env.PORT || 443;
 
-const port = process.env.PORT || 27017;
-//Server should run this if it is working
-server.listen(port, () => {
-  console.log(`I'm up and running on port ${port}`);
-});
+app.use(express.json());
+app.use(cors());
+app.use(
+  cookie({
+    name: "letsmovehomie",
+    maxAge: 24 * 60 * 60 * 1000, //set cookie to one day exp
+    keys: [keys.session.cookieKey],
+    domain: "letsmovehomie.com"
+  })
+);
 
- //From previous code. Checks environment variables
+app.use(express.static(__dirname, { dotfiles: "allow" }));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(cookieParser());
+app.use("/city", city);
+app.use("/users", users);
+app.use("/auth", auth);
+app.use("/profile", profile);
+
+//Connect to MongoDB
+mongoose
+  .connect(keys.mongodb.dbURI, {
+    useNewUrlParser: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log("MongoDB successfully connected."))
+  .catch(e => console.error(`Could not connect: ${e.message}`));
+
+if (credentials) {
+  const server = https.createServer(credentials, app);
+  server.listen(port, () => {
+    console.log("server starting on port : " + port);
+  });
+} else {
+  app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+}
+
 function checkConfig() {
   if (
     !process.env.GOOGLE_CLIENTID ||
