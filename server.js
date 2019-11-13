f6d75d59e765e87dfd9c02c5f8d671e3d9f3467d
Manju KC <kcmanjuthapa@gmail.com>
2019-11-13 12:10:57 -0500

    refactor the code

--- server.js
+++ server.js
@@ -1,35 +1,50 @@
-const express = require('express');
+const express = require("express");
 //Security Imports
-const cors = require('cors');
-const helmet = require('helmet');
+const cors = require("cors");
+const helmet = require("helmet");
 const passport = require("passport");
-//Auth not set up how I would like it done but I'm going to leave it for now so that we don't get any bugs in testing. 
+//Auth not set up how I would like it done but I'm going to leave it for now so that we don't get any bugs in testing.
 //Plan is to have this better organized to my liking by v5
-const authenticate = require('./routes/auth');
+const cookie = require("cookie-session");
+const cookieParser = require("cookie-parser");
+const https = require("https");
 const users = require("./routes/users");
 const auth = require("./routes/auth");
 const city = require("./routes/city");
+const passportConfig = require("./middleware/passportConfig");
 const profile = require("./routes/profile");
-//Still not sure what keys are doing 
+
+//Still not sure what keys are doing
 const keys = require("./config/keys");
 const server = express();
 
-server.use(helmet())
+server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-
+server.use(
+  cookie({
+    name: "letsmovehomie",
+    maxAge: 24 * 60 * 60 * 1000, //set cookie to one day exp
+    keys: [keys.session.cookieKey],
+    domain: "letsmovehomie.com"
+  })
+);
 
 server.use(passport.initialize());
 server.use(passport.session());
+server.use(express.static(__dirname, { dotfiles: "allow" }));
+server.use(passport.initialize());
+server.use(passport.session());
+server.use(cookieParser());
 //Routes
 server.use("/city", city);
 server.use("/users", users);
 server.use("/auth", auth);
 server.use("/profile", profile);
 //Informs devs that this is working
-server.get('/', (req, res) => {
-    res.send("<h1>Let's Move Homie is a terrible name</h1>")
-})
+server.get("/", (req, res) => {
+  res.send("<h1>Let's Move Homie is a terrible name</h1>");
+});
 
 module.exports = server;
