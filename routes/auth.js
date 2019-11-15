0acc9ed4890ac03277d13fc021b7b7c14e8397e2
Lambda_School_Loaner_212 <lambda_school_loaner_212@Lambda-School-Loaner-212s-MacBook-Pro.local>
2019-11-15 12:05:50 -0800

    testing

--- routes/auth.js
+++ routes/auth.js
@@ -20,15 +20,36 @@ router.get(
     res.send("login");
   }
 );
+
+// GET /auth/google/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+// app.get('/auth/google/callback', 
+//   passport.authenticate('google', { failureRedirect: '/login' }),
+//   function(req, res) {
+//     res.redirect('/');
+//   });
+
 router.get(
-  "/login/facebook",
+  "/login/facebook/cb",
   passport.authenticate("facebook", {
-    scope: ["email"]
+    successRedirect: '/',
+    failureRedirect: '/login'
   }),
   (req, res) => {
     res.send("login");
   }
 );
+
+// Facebook will redirect the user to this URL after approval.  Finish the
+// authentication process by attempting to obtain an access token.  If
+// access was granted, the user will be logged in.  Otherwise,
+// authentication has failed.
+// app.get('/auth/facebook/callback',
+//   passport.authenticate('facebook', { successRedirect: '/',
+//                                       failureRedirect: '/login' }));
 
 //clear all sessions of cookies etc
 router.get("/logout", (req, res) => {
@@ -49,20 +70,20 @@ router.get("/redirect/google", passport.authenticate("google"), (req, res) => {
   res.status(303).redirect("https://stagefe.letsmovehomie.com/topten");
 });
 //Redirect url for user
-router.get(
-  "/redirect/facebook",
-  passport.authenticate("facebook"),
-  (req, res) => {
-    console.log(req.cookies["letsmovehomie"]);
-    res.cookie("letsmovehomie", req.cookies["letsmovehomie"], {
-      domain: "letsmovehomie.com"
-    });
-    res.cookie("letsmovehomie.sig", req.cookies["letsmovehomie.sig"], {
-      domain: "letsmovehomie.com"
-    });
-    res.status(303).redirect("https://stagefe.letsmovehomie.com/topten");
-  }
-);
+// router.get(
+//   "/redirect/facebook",
+//   passport.authenticate("facebook"),
+//   (req, res) => {
+//     console.log(req.cookies["letsmovehomie"]);
+//     res.cookie("letsmovehomie", req.cookies["letsmovehomie"], {
+//       domain: "letsmovehomie.com"
+//     });
+//     res.cookie("letsmovehomie.sig", req.cookies["letsmovehomie.sig"], {
+//       domain: "letsmovehomie.com"
+//     });
+//     res.status(303).redirect("https://stagefe.letsmovehomie.com/topten");
+//   }
+// );
 
 //Check for authentication
 router.get("/validation", authCheck, (req, res) => {
