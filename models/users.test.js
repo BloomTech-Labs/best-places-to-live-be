677ac15381542b50c58e5281322dbdda10e8c8fd
Manju KC <kcmanjuthapa@gmail.com>
2019-11-20 01:03:57 -0500

    test the user model two test pass

---
+++ models/users.test.js
@@ -0,0 +1,41 @@
+var chai = require('chai'),
+request = require('supertest'),
+server = require('../server.js')
+model = require('./user')
+
+
+chai.should();
+
+describe('REST API (/users)', function () {
+    var json = {
+        name: 'user',
+        email: 'user@email.com',
+        password: '1234567890',
+        location:'ga',
+        
+      };
+      describe('Schema', function (){
+
+        schema = model.schema.paths;
+
+
+    it('should have property \'name\' correctly', function (done) {
+
+        schema.should.have.property('name');
+        schema.name.options.type.should.be.equal(String);
+  
+        done();
+  
+      });
+      it('should have property \'email\' correctly', function (done) {
+
+        schema.should.have.property('email');
+        schema.name.options.type.should.be.equal(String);
+  
+        done();
+  
+      });
+
+       
+      });    
+});
