4a8c3580d6430efc8f077a708d25ecb90632223e
Manju KC <kcmanjuthapa@gmail.com>
2019-11-20 21:22:05 -0500

    updated some code

--- routes/users.js
+++ routes/users.js
@@ -245,10 +245,9 @@ router.post("/login", async (req, res) => {
 
 // Register Handle
 router.post("/register", async (req, res) => {
-  console.log(req)
   const { name,email,password,location} = req.body;
-  console.log({name,email,password,location})
   // check required fields
+  console.log(name,email,password,location);
   if (!name || !email || !password || !location) {
     res.status(400).json({
       message: "Please fill in all fields."
@@ -294,7 +293,6 @@ router.post("/register", async (req, res) => {
         });
       }
     } catch (error) {
-      console.log(error);
       res.status(500).json({
         message: "Error registering."
       });
