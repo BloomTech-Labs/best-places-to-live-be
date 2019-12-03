4a3633f17228179d21175ea8173f7c1641f3dbb0
Lambda_School_Loaner_212 <lambda_school_loaner_212@Lambda-School-Loaner-212s-MacBook-Pro.local>
2019-12-03 10:50:19 -0800

    Scratched old test and trying something from itnext

--- server.spec.js
+++ server.spec.js
@@ -7,6 +7,7 @@ const User = require("./models/user");
 const keys = require("./config/keys");
 const jwt = require("jsonwebtoken");
 
+
 chai.use(chaiHttp);
 chai.should();
 
@@ -90,61 +91,11 @@ describe('city', () => {
 //             });
 //     });
 })})
-const Register = (body) => {
-    const {name, password} = body;
-    if(!name || !password) {
-        res.status(400).json({
-            message: "Please fill in all fields."
-        });
-    }else if(password.length < 6){
-        res.status(500).json({
-            message: "Password must be at least 6 characters"
-        });
-    }else {
-        try {
-            const user =  User.findOne({name});
-            if(user) {
-                res.status(500).json({
-                    message: "User already exists. Please login to continue"
-                });
-            }else {
-                const hashedPassword = bcrypt.hashSync(password, 10);
-                const newUser = new User({
-                    name, 
-                    password: hashedPassword
-                });
-                const userSaved = newUser.save();
-                const token = jwt.sign(
-                    {
-                        name: userSaved.name,
+descibe('Login', () => {
+    it('succeeds with correct crerdtials', async () => {
+        const response = await post(`login`, demoUser)
+        .expect(200);
+        expect(res.body.user.email).toBe(demoUser.email);
+    });
 
-                    },
-                    keys.jwtAuth.secret,
-                    {expiresIn: "10h"}
-
-                );
-                res.status(200).json({
-                    name: userSaved.name,
-                    token
-                });
-            }
-        } catch (error) {
-            res.status(500).json({
-                message: "Error registering."
-            });
-        }
-    }
-};
-describe("Register", () => {
-    test("should successfully register new user", () => {
-        let user = {name: 'JestTest', password: 'testing123'};
-         expect(Register(user)).resolves.toEqual({
-            validted: true,
-            reason: 'You have successfully registered!',
-            user: {
-                password: expect.anything(),
-                name: 'JestTest'
-            }
-        });
-    })
 })
