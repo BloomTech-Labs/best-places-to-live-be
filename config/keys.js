bbec42be293e2deca1d98ac9ffbe35ceba0037fe
Manju KC <kcmanjuthapa@gmail.com>
2019-12-02 19:27:14 -0500

    fixed the error and able to login with google auth

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
+
+
 module.exports = {
   mongodb: {
     dbURI: `mongodb://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOSTNAME}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}`
@@ -6,10 +8,11 @@ module.exports = {
     cookieKey: process.env.COOKIE_KEY
   },
   googleAuth: {
-    googleClientId: process.env.GOOGLE_CLIENTID,
-    googleClientSecret: process.env.GOOGLE_CLIENTSECRET,
-    callbackURL: '/auth/google/redirect'
-   
+    // googleClientId: process.env.GOOGLE_CLIENTID,
+    // googleClientSecret: process.env.GOOGLE_CLIENTSECRET,
+    googleClinetId:'257506570786-9v5lgvl06hscddcublc4d06vkph9i0q8.apps.googleusercontent.com',
+    googleClientSecret:'QRDix_PJ1-ctZxCyFJozNz_H',
+    callbackURL: '/auth/google/callback' 
   },
   facebookAuth: {
     facebookClientId: '482046309075252',
