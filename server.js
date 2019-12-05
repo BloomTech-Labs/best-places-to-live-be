554ceb16ebe5a875041bd5eac48512c133657adf
Manju KC <kcmanjuthapa@gmail.com>
2019-12-05 09:44:07 -0500

    Updated domain name

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ server.use(
     name: "letsmovehomie",
     maxAge: 24 * 60 * 60 * 1000, //set cookie to one day exp
     keys: [keys.session.cookieKey],
-    // domain: "letsmovehomie.com"
+    domain: "liveinthebestplace.com"
   })
 );
 
