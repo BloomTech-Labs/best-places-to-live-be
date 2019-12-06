0f517a23c4b11c77449ea4c823590f36056d4ad7
Lambda_School_Loaner_212 <lambda_school_loaner_212@Lambda-School-Loaner-212s-MacBook-Pro.local>
2019-12-05 16:57:58 -0800

    finished my server tests

--- config/jest.config.js
+++ config/jest.config.js
@@ -6,15 +6,3 @@ module.exports = {
 
 
 
-module.exports = {
-    mongodbMemoryServerOptions: {
-      instance: {
-        dbName: 'jest'
-      },
-      binary: {
-        version: '4.0.3',
-        skipMD5: true
-      },
-      autoStart: false
-    }
-  };
--- server.spec.js
+++ server.spec.js
@@ -8,6 +8,7 @@ const keys = require("./config/keys");
 const jwt = require("jsonwebtoken");
 const {MongoClient} = require('mongodb');
 
+
 chai.use(chaiHttp);
 chai.should();
 
@@ -56,78 +57,40 @@ describe('users', () => {
                 done();
             });
         });
+        it('should register',  (done) => {
+            const newUser = request(server)
+            .post('users/register')
+            .send({
+                name: "Jest",
+                email: "testing@jest.com",
+                location: "San Fran",
+                password: "1234567"
+            })
+            
+            expect(newUser._data).toEqual({
+                name: "Jest",
+                email: "testing@jest.com",
+                location: "San Fran",
+                password: "1234567"
+            })
+            done()
+        })
+        it('should log in', (done) => {
+            const res = request(server)
+            .post('users/login')
+            .send({
+                email: "testing@jest.com",
+                password:"1234567"
+            })
+            console.log(res._data)
+            expect(res._data).toEqual({
+                email: "testing@jest.com",
+                password:"1234567"
+            })
+            done()
+        })
     });
 });
-// describe('profile', () => {
-//     describe('GET /', () => {
-//         it('should get NOT user info', (done) => {
-//             chai.request(server)
-//             .get('/profile')
-//             .end((err, res) => {
-//                 expect(res.status).toEqual(400);
-//                 done();
-//             });
-//         });
-//         it('should not be able to update user information', (done) => {
-//             chai.request(server)
-//             .put('/profile')
-//             .end((err, res) => {
-//                 expect(res.status).toEqual(401);
-//                 done();
-//             });
-//         });
-//     })
-// })
-// describe('city', () => {
-//     describe('GET /', () => {
-//         it('should get city info',  (done) => {
-//             chai.request(server)
-//             .get('/city/all')
-//             .end((err, res) => {
-//                 expect(res.status).toEqual(200);
-//                 done();
-//             });
-//         });
-//         it('should get top ten', async (done) => {
-//             chai.request(server)
-//             .get('/city/topten-score_total')
-//             .end((err, res) => {
-//                 expect(res.status).toEqual(200);
-//                 done();
-//             });
-//     });
-// })})
-describe('insert', () => {
-    let connection;
-    let db;
 
-    beforeAll(async () => {
-        connection = await MongoClient.connect(global._MONGO_URL_, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        db = await connection.db();
-    });
-    afterAll(async () => {
-        await connection.close();
-        
-    });
-    it('should insert a doc into the collection', async () => {
-        const users = db.collection('users');
-        const mockUser = {_id: 'some-user-id', name: 'John'};
-        await users.insertOne(mockUser);
-        const insertedUser = await users.findOne({_id: 'some-user-id'});
-        expect(insertedUser).toEqual(mockUser);
-    });
-    beforeEach(async () => {
-        await db.collection('COLLECTION_NAME').deleteMany({});
-      });
-});
 
-function post(url, body){
-    const httpRequest = request(app).post(url);
-    httpRequest.send(body);
-    httpRequest.set('Accept', 'application/json')
-    httpRequest.set('Origin', 'http://localhost:300')
-    return httpRequest;
-}
+
