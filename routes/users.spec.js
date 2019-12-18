f15101a6e46c0f7d2e7870ea7ca4d43007134226
Manju KC <kcmanjuthapa@gmail.com>
2019-12-18 14:02:13 -0500

    signup router tests are passed

--- routes/users.spec.js
+++ routes/users.spec.js
@@ -115,10 +115,67 @@ describe("signup router", () => {
         name: "Fredo",
         email: "kcmanjuthapa@gmail.com",
         password: "testing",
-        location: "Atlanta"
+        location: "Atlanta",
+        AppleId: "1235467890"
       };
       supertest(router)
         .post("/register")
+        .send(data)
+        .expect(400)
+        .expect("Content-Type", /json/i);
+    });
+    it("should responds with 400 Bad Response", () => {
+      const data = {
+        name: "",
+        email: "kcmanjuthapa@gmail.com",
+        password: "",
+        location: "Atlanta",
+        appleId: ""
+      };
+      supertest(router)
+        .post("/signup")
+        .send(data)
+        .expect(400)
+        .expect("Content-Type", /json/i);
+    });
+    it("should response with 400 Bad Response", () => {
+      const data = {
+        name: "Fredo",
+        email: "",
+        password: "",
+        location: "",
+        appleId: "1234567890"
+      };
+      supertest(router)
+        .post("/signup")
+        .send(data)
+        .expect(400)
+        .expect("Content-Type", /json/i);
+    });
+    it("should response with 400 Bad Response", () => {
+      const data = {
+        name: "Fredo",
+        email: "",
+        password: "",
+        location: "",
+        appleId: ""
+      };
+      supertest(router)
+        .post("/signup")
+        .send(data)
+        .expect(400)
+        .expect("Content-Type", /json/i);
+    });
+    it("should response with 400 Bad Response", () => {
+      const data = {
+        name: "",
+        email: "",
+        password: "",
+        location: "",
+        appleId: ""
+      };
+      supertest(router)
+        .post("/signup")
         .send(data)
         .expect(400)
         .expect("Content-Type", /json/i);
